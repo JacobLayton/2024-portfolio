0eaadd76d5cb5ef498502019606613711f0c96c3
Jacob Layton <jlayjlay3@gmail.com>
2024-01-03 20:07:48 -0800

    Update mobile about section styling

--- app/ui/about.jsx
+++ app/ui/about.jsx
@@ -8,13 +8,15 @@ export default async function About() {
 			<p>
 				Born and raised in Portland, Oregon - I appreciate all that the Pacific Northwest
 				has to offer. Whether it's snowboarding on Mt. Hood or hiking through the Columbia
-				River Gorge, I love to get out and experience as much as possible. I bring the same
-				disposition to the world of coding. I'm passionate about learning new technoglogies,
-				understanding how they work under the hood, and using them creatively to produce
-				something new and different.
+				River Gorge, I love to get out and experience as much as possible.
 			</p>
 			<p>
-				In my free time I play basketball and lacrosse, go snowbaording, fly my drone, and
+				I bring the same disposition to the world of coding. I'm passionate about learning
+				new technoglogies, understanding how they work under the hood, and using them
+				creatively to produce something new and different.
+			</p>
+			<p>
+				In my free time I play basketball and lacrosse, go snowboarding, fly my drone, and
 				dabble in music and video production.
 			</p>
 		</div>
