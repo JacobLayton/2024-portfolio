6ca3d5e7dd58192f753db157a3bff97af7446711
JacobLayton <jlayjlay3@gmail.com>
2025-05-05 17:32:50 -0700

    Set up toast notification for dead linked cards

--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
+'use client';
 import Image from 'next/image';
 import Nav from '@/app/ui/nav';
 import About from '@/app/ui/about';
 import Experience from '@/app/ui/experience';
 import Projects from '@/app/ui/projects';
 import ContactForm from '@/app/ui/contact-form';
+import { ToastContainer } from 'react-toast';
 import styles from './page.module.css';
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
+			<ToastContainer position={'bottom-right'} />
 			<div className={styles.topOrLeft}>
 				<Nav />
 			</div>
