9d26b7642cb470fe9b13e0a9aae554446fa2610d
JacobLayton <jlayjlay3@gmail.com>
2025-05-13 22:04:42 -0700

    Add scroll nav highlighter

--- app/ui/nav.jsx
+++ app/ui/nav.jsx
@@ -19,6 +19,35 @@ export default function Nav() {
 	function handleClick(section) {
 		updateActiveLink(section);
 	}
+	React.useEffect(() => {
+		const sectionIds = ['about', 'experience', 'projects', 'contact'];
+		const threshold = 200; // highlight sooner
+		const bottomOffset = 100; // px from bottom to force 'contact'
+		const handleScroll = () => {
+			const scrollPosition = window.innerHeight + window.scrollY;
+			const pageHeight = document.body.offsetHeight;
+			// If near bottom, always highlight contact
+			if (pageHeight - scrollPosition < bottomOffset) {
+				updateActiveLink('contact');
+				return;
+			}
+			let found = false;
+			for (let i = sectionIds.length - 1; i >= 0; i--) {
+				const section = document.getElementById(sectionIds[i]);
+				if (section) {
+					const rect = section.getBoundingClientRect();
+					if (rect.top <= threshold) {
+						updateActiveLink(sectionIds[i]);
+						found = true;
+						break;
+					}
+				}
+			}
+			if (!found) updateActiveLink(sectionIds[0]);
+		};
+		window.addEventListener('scroll', handleScroll, { passive: true });
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
 	return (
 		<div className={styles.nav}>
 			<div className={styles.infoAndLinks}>
@@ -67,6 +96,22 @@ export default function Nav() {
 				</div>
 			</div>
 			<div className={styles.icons}>
+				<a
+					href='https://www.linkedin.com/in/jacob-layton/'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						x='0px'
+						y='0px'
+						width='100'
+						height='100'
+						viewBox='0 0 30 30'
+					>
+						<path d='M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z'></path>
+					</svg>
+				</a>
 				<a href='https://github.com/JacobLayton' target='_blank' rel='noopener noreferrer'>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
@@ -93,22 +138,6 @@ export default function Nav() {
 						viewBox='0 0 32 32'
 					>
 						<path d='M 11.46875 5 C 7.917969 5 5 7.914063 5 11.46875 L 5 20.53125 C 5 24.082031 7.914063 27 11.46875 27 L 20.53125 27 C 24.082031 27 27 24.085938 27 20.53125 L 27 11.46875 C 27 7.917969 24.085938 5 20.53125 5 Z M 11.46875 7 L 20.53125 7 C 23.003906 7 25 8.996094 25 11.46875 L 25 20.53125 C 25 23.003906 23.003906 25 20.53125 25 L 11.46875 25 C 8.996094 25 7 23.003906 7 20.53125 L 7 11.46875 C 7 8.996094 8.996094 7 11.46875 7 Z M 21.90625 9.1875 C 21.402344 9.1875 21 9.589844 21 10.09375 C 21 10.597656 21.402344 11 21.90625 11 C 22.410156 11 22.8125 10.597656 22.8125 10.09375 C 22.8125 9.589844 22.410156 9.1875 21.90625 9.1875 Z M 16 10 C 12.699219 10 10 12.699219 10 16 C 10 19.300781 12.699219 22 16 22 C 19.300781 22 22 19.300781 22 16 C 22 12.699219 19.300781 10 16 10 Z M 16 12 C 18.222656 12 20 13.777344 20 16 C 20 18.222656 18.222656 20 16 20 C 13.777344 20 12 18.222656 12 16 C 12 13.777344 13.777344 12 16 12 Z'></path>
-					</svg>
-				</a>
-				<a
-					href='https://www.linkedin.com/in/jacob-layton/'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						x='0px'
-						y='0px'
-						width='100'
-						height='100'
-						viewBox='0 0 30 30'
-					>
-						<path d='M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z'></path>
 					</svg>
 				</a>
 			</div>
