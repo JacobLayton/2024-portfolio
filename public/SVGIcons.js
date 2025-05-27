420c26a13f87ab29de8051263b42fc9f48a1fdd2
JacobLayton <jlayjlay3@gmail.com>
2025-05-26 17:20:54 -0700

    Update launch arrow styles

--- public/SVGIcons.js
+++ public/SVGIcons.js
@@ -45,9 +45,9 @@ export const LaunchSVG = (
 		xmlns='http://www.w3.org/2000/svg'
 		x='0px'
 		y='0px'
-		width='35'
-		height='35'
-		viewBox='0 0 32 32'
+		width='20'
+		height='20'
+		viewBox='0 0 20 20'
 	>
 		<path d='M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z'></path>
 	</svg>
